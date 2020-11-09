99c6aa94e6f1b759d857346e083477343858b392
Logan <loganroyer@hotmail.com>
2020-11-09 12:50:39 -0800

    Create Person.h
    Header file for the person class. Contains Name, Age and Gender and basic functions.

---
+++ Person.h
@@ -0,0 +1,41 @@
+/*
+* Title: COMP 3140 Team Project Stage 1
+* File: Person.h
+* Description: Team project part 1, header file for the Person class
+*
+* Author: Logan Royer
+* Student ID: T00215409
+* Date: November 9th, 2020
+*/
+
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+using namespace std;
+using std::string; 
+
+class Person{
+protected:
+  string name;
+  string gender;
+  int age;
+  
+public:
+  Person ();
+  Person (string name, string gender, int age);
+  Person (const Person &pOld);
+  ~Person ()
+
+  string getName ();
+  string getGender ();
+  int getAge ();
+  
+  void setName (string name);
+  void setGender (string gender);
+  void setAge (int age);
+  
+  string getPerson ();
+};
+
+#endif
