fa2637399607fd925e33054f5623e2214c41c223
Shane <steiner5676@gmial.com>
2020-11-09 21:04:55 -0800

    replace original Person.h

--- Person.h
+++ Person.h
@@ -1,36 +1,46 @@
-#pragma once
-#pragma once
-#include "Person.h"
-#include <iostream>
-
 /*
-- Team Project: Stage #1
-- Dayton Butler
-- T00258753
-- November 12, 2020
-- COMP 3140
+* Title: COMP 3140 Team Project Stage 1
+* File: Person.h
+* Description: Team project part 1, header file for the Person class
+*
+* Author: Logan Royer
+* Student ID: T00215409
+* Date: November 9th, 2020
 */
 
-using namespace std;
+#ifndef PERSON_H
+#define PERSON_H
 
-class Person
-{
-private:
-	string name;
-	int age;
-	string gender;
+#include <string>
+using namespace std;
+using std::string; 
+
+class Person{
+protected:
+  string name;
+  string gender;
+  int age;
+  
 public:
-	// Constructors/Destructors
-	Person();
-	Person(string name, int age, string gender);
-	Person(const Person& person);
-	virtual ~Person();
-	// Getters/Setters
-	string getName() const;
-	int getAge() const;
-	string getGender() const;
-	void setName(string name);
-	void setAge(int age);
-	void setGender(string gender);
-	virtual void profile();
+  
+  /* Constructors */
+  Person ();
+  Person (string name, string gender, int age);
+  Person (const Person &pOld);
+  ~Person ();
+
+  /* Getters and Setters */
+  string getName ();
+  string getGender ();
+  int getAge ();
+  
+  void setName (string name);
+  void setGender (string gender);
+  void setAge (int age);
+  
+  /* Member Functions */
+  //gets a string representation of the person
+  string getPerson ();
 };
+
+#endif
