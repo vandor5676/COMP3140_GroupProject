b1fba61c3c5c76e9e09cd7926b9682583d2197a4
Shane <steiner5676@gmial.com>
2020-11-19 13:47:35 -0800

    added coments

--- Manager.h
+++ Manager.h
@@ -15,28 +15,61 @@ using namespace std;
 class Manager : public Person
 {
     private:
+        //date manager was hired using a Date object
         Date hired_date;
+
+        // the managers salary
         double salary;
+
+        // the managers bonus 
         double bouns;
+
+        //the managers monthly expences
         double Monthly_expense;
     public:
+
+        //
         //Constructors
+        //
+
+        //creates a new manager with a name, age, gender, hiredate, dalary, bonus and monthly expence
         Manager(const string& newName,const int& newAge,
         const string& newGender,const Date& newHired_date,
         const double& newSalary,const double& newBouns,
         const double& newMonthly_expense);
+
+        //creates a new manager
         Manager();
+
+        //
         //Destructors
+        //
+
+        //deconstructs a manager
         virtual ~Manager();
+
+        //
         //Mutators
+        //
+
+        // sets the hired date
         void setHired_Date(const Date& date);
+        //sets the salary
         void setSalary(const double& newSalary);
+        //sets the bonus
         void setBouns(const double& newBouns);
+        //sets the monthly expence
         void setMonthly_expense(const double& new_me);
+        //
         //Accessors
+        //
+        //gets the hired date
         Date getHired_Date(Date date);
+        //gets the salary
         double getSalary(double newSalary);
+        //gets the bonus
         double getBouns(double newBouns);
+        // getst the monthly expence
         double getMonthly_expense(double new_me);
         //Print information of Manager
         virtual void printInfo() const;
