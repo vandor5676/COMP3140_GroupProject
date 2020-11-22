96958452db6b2657579aea1621188898546a6c8b
drb310 <56737743+drb310@users.noreply.github.com>
2020-11-22 10:57:27 -0800

    Add files via upload

--- Tenant.cpp
+++ Tenant.cpp
@@ -1,66 +1,79 @@
-/*
- *Tititle: COMP3140 Project Stage 2
- *File name: Tenant.cpp
- *File Type: Imlementation File 
- *Date: 11/16/2020
- */
-
 #include "Tenant.h"
 #include <iostream>
-#ifndef DATE_DEF
-#include "Date.cpp"
-#endif
 
 //Constructors
-Tenant::Tenant(const string &newName, const int &newAge,
-               const string &newGender, const string &newJob,
-               const int &newUnitNumber, Date newMoveIn_Day,
-               const double &newMonthly_rent, const char &newPayment_status)
-:Person(newName,newAge,newGender), job(newJob),unitNumber(newUnitNumber), monthly_rent(newMonthly_rent)
+Tenant::Tenant(const string& newName, const int& newAge,
+	const string& newGender, const string& newJob,
+	const int& newUnitNumber, Date newMoveIn_Day,
+	const double& newMonthly_rent, const char& newPayment_status)
 {
-    this->movingIn_Date = Date(newMoveIn_Day);
-   //  it looks like we cant use newPayment_status, im going to vreate a list in ManagmentExtraFunctions that contains the information that shoud go here
-  // this->payment_status[1] = 'a'; 
+	this->job = newJob;
+	this->unitNumber = newUnitNumber;
+	this->movingIn_Date = Date(newMoveIn_Day);
+	this->monthly_rent = newMonthly_rent;
 }
+
 Tenant::Tenant()
 {
+
 }
+
 //Destructors
 Tenant::~Tenant()
 {
+
 }
+
 //Mutators
-void Tenant::setJob(const string &newJob)
+void Tenant::setJob(const string& newJob)
 {
+	this->job = newJob;
 }
-void Tenant::setUnitNumber(const int &newUnitNumber)
+
+void Tenant::setUnitNumber(const int& newUnitNumber)
 {
+	this->unitNumber = newUnitNumber;
 }
-void Tenant::setMonthlyRent(const double &newMonthly_rent)
+
+void Tenant::setMonthlyRent(const double& newMonthly_rent)
 {
+	this->monthly_rent = newMonthly_rent;
 }
-void Tenant::setPaymentStatus(const char &newPayment_status)
+
+void Tenant::setPaymentStatus(const char& newPayment_status)
 {
+	
 }
+
 //Accessors
 string Tenant::getJob() const
 {
+	return job;
 }
+
 int Tenant::getUnitNumber() const
 {
+	return unitNumber;
 }
+
 Date Tenant::getmovingIn_Date() const
 {
+	return movingIn_Date;
 }
+
 double Tenant::getMonthlyRent() const
 {
-    return(this->monthly_rent);
+	return monthly_rent;
 }
-char *Tenant::getPaymentStatus() const
+
+char* Tenant::getPaymentStatus() const
 {
+	
 }
+
 //Print information of Tenant
 void Tenant::printInfo() const
 {
-    cout<<"Not implemented yet"<<endl;
+	cout << "Tenant Info:" << endl;
+
 }
