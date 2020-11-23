f79b95e1f11d8bb7def4a2d779a0b10c9fec82a7
Shane <steiner5676@gmial.com>
2020-11-23 13:10:04 -0800

    Cherry pick atempt

--- Driver.cpp
+++ Driver.cpp
@@ -8,17 +8,10 @@
 
 #include <iostream>
 #include <string>
-// #include "ManagementExtraFunctions.h" 
-// #include "Date.h"
-// #include "Tenant.h" 
-
-#include "Management.cpp"   
-#include "Manager.cpp" 
-#include "ManagementExtraFunctions.cpp"   
-#include "Date.cpp"
-#include "Tenant.cpp" 
-#include "Person.cpp"
-using namespace std; 
+#include "ManagementExtraFunctions.h"
+#include "Date.h"
+#include "Tenant.h"
+using namespace std;
 
 int main()
 {
@@ -106,7 +99,7 @@ int main()
     }
 
     bool LoganTest = true;
-    if(LoganTest){ 
+    if(LoganTest){
         ManagementExtraFunctions management2("Managers.csv", "Tenants.csv");
         management2.collectRentalFee();
         cout << "The profiles of all managers: " << endl;
